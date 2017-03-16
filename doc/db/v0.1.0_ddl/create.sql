217b260dc067c276634046e6067cb0eb8552b3d7
wangao029@163.com <wangao029@163.com>
2017-03-16 23:29:22 +0800

    增加部分sql 的注释

--- doc/db/v0.1.0_ddl/create.sql
+++ doc/db/v0.1.0_ddl/create.sql
@@ -2,19 +2,21 @@
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE DATABASE IF NOT EXISTS easymall default charset utf8 COLLATE utf8_general_ci;
+
 -- ----------------------------
 --  Table structure for `u_resources`
 -- ----------------------------
-DROP TABLE IF EXISTS `u_resources`;
+DROP TABLE IF EXISTS `u_resources`; COMMENT '资源表'
 CREATE TABLE `u_resources` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `code` varchar(255) DEFAULT NULL,
-  `parent_code` VARCHAR(255) DEFAULT NULL,
-  `type` INT DEFAULT null,
-  `path` varchar(500) DEFAULT NULL,
-  `domain` varchar(255) DEFAULT NULL,
-  `context` VARCHAR(255) DEFAULT null,
-  `remark` VARCHAR(3000) DEFAULT null,
+  `code` varchar(255) DEFAULT NULL COMMENT '标识编号',
+  `parent_code` VARCHAR(255) DEFAULT NULL COMMENT '父编号',
+  `type` INT DEFAULT null COMMENT '类型，1：菜单；2：按钮；3：API接口',
+  `path` varchar(500) DEFAULT NULL COMMENT '路径',
+  `domain` varchar(255) DEFAULT NULL COMMENT '域',
+  `context` VARCHAR(255) DEFAULT null COMMENT '上下文环境',
+  `remark` VARCHAR(3000) DEFAULT null COMMENT '备注',
   `is_delete` int DEFAULT null COMMENT '正常数据为null 删除为非null',
   `create_time` date,
   `create_user` bigint,
@@ -29,10 +31,10 @@ CREATE TABLE `u_resources` (
 --  Table structure for `u_role`
 -- ----------------------------
 DROP TABLE IF EXISTS `u_role`;
-CREATE TABLE `u_role` (
+CREATE TABLE `u_role`(
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `remark` VARCHAR(3000) DEFAULT null,
+  `name` varchar(255) DEFAULT NULL COMMENT '角色名称',
+  `remark` VARCHAR(3000) DEFAULT null COMMENT '备注',
   `is_delete` int DEFAULT null COMMENT '正常数据为null 删除为非null',
   `create_time` date,
   `create_user` bigint,
@@ -41,7 +43,7 @@ CREATE TABLE `u_role` (
   `modify_user` bigint,
   `modify_user_name` varchar(255),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8  COMMENT '角色表';
 
 -- ----------------------------
 --  Table structure for `u_role_resources`
@@ -59,17 +61,17 @@ CREATE TABLE `u_role_resources` (
   `modify_user` bigint,
   `modify_user_name` varchar(255),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '角色资源关系表';
 
 -- ----------------------------
 --  Table structure for `u_admin`
 -- ----------------------------
 DROP TABLE IF EXISTS `u_admin`;
-CREATE TABLE `u_user` (
+CREATE TABLE `u_admin` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `salt` VARCHAR(255) not null,
+  `name` varchar(255) NOT NULL COMMENT '用户名',
+  `password` varchar(64) NOT NULL COMMENT '用户密码',
+  `salt` VARCHAR(255) not null COMMENT '加密盐',
   `is_delete` int DEFAULT null COMMENT '正常数据为null 删除为非null',
   `create_time` date,
   `create_user` bigint,
@@ -85,7 +87,7 @@ CREATE TABLE `u_user` (
 --  Table structure for `u_admin_role`
 -- ----------------------------
 DROP TABLE IF EXISTS `u_admin_role`;
-CREATE TABLE `u_user_role` (
+CREATE TABLE `u_admin_role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) DEFAULT NULL,
   `role_id` bigint(20) DEFAULT NULL,
@@ -97,17 +99,18 @@ CREATE TABLE `u_user_role` (
   `modify_user` bigint,
   `modify_user_name` varchar(255),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '用户角色关系';
 
 
 DROP TABLE IF EXISTS `u_user`;
 CREATE TABLE `u_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `mobile` varchar(255) DEFAULT NULL,
-  `password` varchar(64) DEFAULT NULL,
-  `salt` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL COMMENT '用户名',
+  `real_name` varchar(255) DEFAULT NULL COMMENT '用户名',
+  `email` varchar(255) DEFAULT NULL COMMENT '邮箱',
+  `mobile` varchar(255) DEFAULT NULL COMMENT '电话',
+  `password` varchar(64) DEFAULT NULL COMMENT 'MD5密码',
+  `salt` varchar(255) DEFAULT NULL COMMENT '加密盐',
   `is_delete` int DEFAULT null COMMENT '正常数据为null 删除为非null',
   `create_time` date,
   `create_user` bigint,
@@ -136,7 +139,7 @@ CREATE TABLE `u_user_cookie` (
   `modify_user` bigint,
   `modify_user_name` varchar(255),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='c端用户表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户cookie';
 
 DROP TABLE IF EXISTS `p_attribute_name`;
 CREATE TABLE `p_attribute_name` (
