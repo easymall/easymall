54cf124fcca4ce3a866bd07e2930d85a8d82389e
zhu8fei <zhu8fei@gmail.com>
2017-03-28 23:16:47 +0800

    测试工具,mybatis通用工具

--- common/src/main/java/com/zhu8fei/common/mybatis/plugin/InsertSqlPlugin.java
+++ common/src/main/java/com/zhu8fei/common/mybatis/plugin/InsertSqlPlugin.java
@@ -1,7 +1,16 @@
 package com.zhu8fei.common.mybatis.plugin;
 
+import com.zhu8fei.common.base.ModelBase;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Signature;
+
+import java.util.concurrent.Executor;
+
 /**
  * Created by zhu8fei on 2017/3/28.
  */
-public class InsertSqlPlugin {
+@Intercepts({@Signature(type = Executor.class, method = "insert",
+        args = {ModelBase.class, Object.class})})
+public class InsertSqlPlugin extends CommonSqlPlugin{
+
 }
--- common/src/test/java/com/zhu8fei/common/mybatis/plugin/InsertSqlPluginTest.java
+++ common/src/test/java/com/zhu8fei/common/mybatis/plugin/InsertSqlPluginTest.java
@@ -1,8 +1,16 @@
+package com.zhu8fei.common.mybatis.plugin;
+
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by zhu8fei on 2017/3/28.
  */
 public class InsertSqlPluginTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
 
 }
