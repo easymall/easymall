795f72e963acaeb1c90022307f17de5b988a7f7b
zhu8fei <zhu8fei@gmail.com>
2017-04-24 22:58:46 +0800

    基本测试功能.

--- common/src/test/java/com/zhu8fei/common/mybatis/plugin/InsertSqlPluginTest.java
+++ common/src/test/java/com/zhu8fei/common/mybatis/plugin/InsertSqlPluginTest.java
@@ -21,7 +21,7 @@ import static org.junit.Assert.*;
  * Created by zhu8fei on 2017/3/28.
  */
 public class InsertSqlPluginTest extends UnitilsJUnit4 {
-    @SpringApplicationContext({"application-test.xml"})
+    @SpringApplicationContext({"application-test.xml","unitils-datasource.xml"})
     ApplicationContext context;
 
     @SpringBean("userMapper")
