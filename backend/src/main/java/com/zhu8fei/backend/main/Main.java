8f98f51d328ba5e7c3f6f2685bb374e8e545ffe2
zhu8fei <zhu8fei@gmail.com>
2017-05-01 14:44:33 +0800

    mac上测试没有成功. 放弃.

--- backend/src/main/java/com/zhu8fei/backend/main/Main.java
+++ backend/src/main/java/com/zhu8fei/backend/main/Main.java
@@ -6,13 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 /**
- * Created by cwx on 2017/4/27.
+ * 主启动类
+ * Created by zhu8fei on 2017/4/
  */
 @EnableAutoConfiguration
 public class Main {
@@ -20,52 +17,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         NDC.push("      MAIN-THREAD");
-        init();
         SpringApplication.main(args);
         NDC.clear();
-    }
-
-    private static void init() {
-
-        File path = new File(System.getProperty("user.dir") + File.separator + "backend" + File.separator + "gulpfile.js");
-        if (path.exists()) {
-            String os = System.getProperty("os.name").toLowerCase();
-            String pre = "";
-            String nvm = "nvm";
-            String npm = "cnpm";
-            String gulp = "gulp";
-            String pipe = " | ";
-            String win = "";
-            if (os.indexOf("windows") >= 0) {
-                nvm = "nvmw";
-                pre = "cmd /c ";
-                pipe = " && ";
-                win = pipe +path.getAbsolutePath().split(":")[0].toLowerCase() +": "+pipe;
-            }
-            String commandMove =  " cd " + path.getParent();
-            String commandUseNode = nvm + " use node ";
-            String commandNpmInstall = npm + " install --save ";
-            String commandGulp = gulp;
-
-            String command = pre + win + commandMove + pipe + commandUseNode + pipe + commandNpmInstall + pipe + commandGulp;
-            try {
-                execute(command);
-            } catch (Exception e) {
-                logger.error(e.getMessage(), e);
-            }
-        }
-    }
-
-    private static void execute(String command) throws Exception {
-        Runtime runtime = Runtime.getRuntime();
-        Process proc = runtime.exec(command);
-        InputStream stderr = proc.getInputStream();
-        InputStreamReader isr = new InputStreamReader(stderr);
-        BufferedReader br = new BufferedReader(isr);
-        String line;
-        while ((line = br.readLine()) != null)
-            logger.info(line);
-        int exitVal = proc.waitFor();
-        logger.info("execute result {}", exitVal);
     }
 }
