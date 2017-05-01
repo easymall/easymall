647c08b65a63936eea946f81641a147eaec14af2
zhu8fei <zhu8fei@gmail.com>
2017-05-01 14:45:31 +0800

    mac上测试没有成功. 放弃.

--- backend/src/main/java/com/zhu8fei/backend/main/Main.java
+++ backend/src/main/java/com/zhu8fei/backend/main/Main.java
@@ -17,6 +17,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         NDC.push("      MAIN-THREAD");
+        logger.info("main class start");
         SpringApplication.main(args);
         NDC.clear();
     }
