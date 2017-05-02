a5baca3fdb9d83210bed28b40f69f09c3987aee5
zhu8fei <zhu8fei@gmail.com>
2017-05-02 10:25:11 +0800

    shiro

---
+++ security/src/main/java/com/zhu8fei/security/crypto/AesEncoder.java
@@ -0,0 +1,9 @@
+package com.zhu8fei.security.crypto;
+
+/**
+ *
+ * Created by zhu8fei on 2017/5/2.
+ */
+public class AesEncoder {
+
+}
---
+++ user/src/test/java/com/zhu8fei/user/login/LoginTest.java
@@ -0,0 +1,11 @@
+package com.zhu8fei.user.login;
+
+/**
+ * Created by zhu8fei on 2017/5/2.
+ */
+public class LoginTest {
+
+    public void test() {
+
+    }
+}
