68b7d4eecc2ac568e79a7b18ebdf137fadff6a4e
zhu8fei <zhu8fei@gmail.com>
2017-05-04 18:01:37 +0800

    mybatis 自动生成 .

---
+++ framework/common/src/main/java/com/zhu8fei/framework/common/exception/CommonException.java
@@ -0,0 +1,29 @@
+package com.zhu8fei.framework.common.exception;
+
+/**
+ * Created by zhu8fei on 2017/5/4.
+ */
+public class CommonException extends RuntimeException {
+
+    public CommonException() {
+        super();
+    }
+
+    public CommonException(String message) {
+        super(message);
+    }
+
+    public CommonException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CommonException(Throwable cause) {
+        super(cause);
+    }
+
+    protected CommonException(String message, Throwable cause,
+                              boolean enableSuppression,
+                              boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
--- framework/common/src/main/java/com/zhu8fei/framework/common/mybatis/config/MybatisConfig.java
+++ framework/common/src/main/java/com/zhu8fei/framework/common/mybatis/config/MybatisConfig.java
@@ -1,14 +1,15 @@
 package com.zhu8fei.framework.common.mybatis.config;
 
 import com.github.pagehelper.PageInterceptor;
+import com.zhu8fei.framework.common.exception.CommonException;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
@@ -20,6 +21,7 @@ import java.util.Properties;
  * Created by zhu8fei on 2017/5/4.
  */
 public class MybatisConfig implements TransactionManagementConfigurer {
+    Logger logger = LoggerFactory.getLogger(MybatisConfig.class);
     @Autowired
     DataSource dataSource;
 
@@ -31,24 +33,20 @@ public class MybatisConfig implements TransactionManagementConfigurer {
 
         //分页插件
         PageInterceptor pageHelper = new PageInterceptor();
+        //FIXME 配置没有修改并不是同一个page插件, 通用字段插件没有引入
         Properties properties = new Properties();
         properties.setProperty("reasonable", "true");
         properties.setProperty("supportMethodsArguments", "true");
         properties.setProperty("returnPageInfo", "check");
         properties.setProperty("params", "count=countSql");
         pageHelper.setProperties(properties);
-
         //添加插件
         bean.setPlugins(new Interceptor[]{pageHelper});
-
-        //添加XML目录
-        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         try {
-            bean.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
             return bean.getObject();
         } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+            logger.error(e.getMessage(), e);
+            throw new CommonException(e);
         }
     }
 
