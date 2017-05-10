3a51b84700aa0782a8d896233679520f0a8a21c5
zhu8fei <zhu8fei@gmail.com>
2017-05-10 10:37:06 +0800

    降低级别.

--- easymall-framework/easymall-core/src/main/java/com/zhu8fei/framework/core/system/trace/Trace.java
+++ easymall-framework/easymall-core/src/main/java/com/zhu8fei/framework/core/system/trace/Trace.java
@@ -10,7 +10,7 @@ public class Trace {
     private final String threadTrace;
 
     public Trace() {
-        // FIXME UUID 算法性能略差,复杂度比较高,比较适合加密使用  此处需要修改为 高效随机算法. 粗略尝试,MAC pro 计算结果为300k dps
+        // XXX UUID 算法性能略差,复杂度比较高,比较适合加密使用  此处需要修改为 高效随机算法. 粗略尝试,MAC pro 计算结果为300k dps
         threadTrace = UUID.randomUUID().toString();
     }
 
